336d22a60e91df2ee4f2d83becc33a07e115ebac
Wolfst0rm <xMakeItRain94@gmail.com>
2017-12-04 15:20:59 +0100

    [DEV 0.1a] Fixes for Logoin
    I am the biggest Derp EVER!!!! Fixed a key issue in regards to Login Auth etc.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const Discord = require('discord.js');
 let client = new Discord.Client();
-let config = require('./config-example.json');
-
-client.login(config.token);
+let config = require('./config.json');
+let prefix = "+";
 
 client.on('ready', () => {
-    console.log('Logged in as ${client.user.tag}!');
+    console.log('Logged in as ' + client.user.tag + ' !');
 });
 
-client.on('message', msg => {
-    if (msg.content === '+ping') {
-    msg.reply('Pong.... PONG...... PON-----!');
-}
+client.on('message', msg =>
+{
+    if(msg.content.startsWith(prefix + 'ping')) {
+        msg.reply('Pong.... PONG...... PON-----!');
+    }
 });
+
+client.login(config.token);
